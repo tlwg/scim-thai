fc10614cab725e48aeaa38d79f1d65f1ef499c30
nobody <nobody@a5fb31c3-a08e-4da0-8e2f-3e0abb528f2e>
2006-07-16 13:35:46 +0000

    This commit was manufactured by cvs2svn to create tag 'r_0_0_0'.

---
+++ tags/r_0_0_0/src/scim_thai_imengine.h
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2006 Theppitak Karoonboonyanan <thep@linux.thai.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef __SCIM_THAI_IMENGINE_H__
+#define __SCIM_THAI_IMENGINE_H__
+
+#include <scim.h>
+
+using namespace scim;
+
+class ThaiIMEngineInstance : public IMEngineInstanceBase
+{
+public:
+    ThaiIMEngineInstance (ThaiIMEngineFactory*  factory,
+                          const String&         encoding,
+                          int                   id = -1);
+    virtual ~ThaiIMEngineInstance ();
+
+    virtual bool process_key_event             (const KeyEvent& key);
+    virtual void reset                         (void);
+
+private:
+    ThaiIMEngineFactory*    m_factory;
+
+    KeyEvent                m_prev_key;
+};
+#endif /* __SCIM_THAI_IMENGINE_H__ */
+/*
+vi:ts=4:nowrap:ai:expandtab
+*/
